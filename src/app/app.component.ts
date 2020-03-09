c4108c14c81c6eab2555594a8fbea597f1d78ee4
W <w@Ws-MacBook-Pro.local>
2020-03-09 02:43:24 -0500

    Changed tsconfig target es2015 -> es5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var device
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    document.addEventListener("deviceready", function() {
+      alert("hi");
+      }, false);
+  }
   title = 'wrg-auto-ag';
 }
